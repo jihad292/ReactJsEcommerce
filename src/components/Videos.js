4bb59b3fd92e0f3c6201fff6edf6b61403e0366e
jihad292 <jihad_chamoun1221@outlook.com>
2021-11-15 18:22:15 +0200

    added first video page

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -9,14 +9,59 @@ export const Videos = ({user})=>{
         <div>
             <Navbar user={user} />
             <h1>Videos</h1>
-            <div className="VideoContainer">
-            <ReactPlayer 
-            controls
-            width={"100%"}
-            height={"100%"}
-            url='https://firebasestorage.googleapis.com/v0/b/first-ecommerce-c5c38.appspot.com/o/videos%2FGIEP8987%20(1).MP4?alt=media&token=d84ebcda-1dcc-4db6-a771-d0763515316c' />
+            <div className="generalDescription">
+                <p>
+                Whether you’re a girl or a boy, young or old, it’s your decision to change yourself and start your 
+                transformation. Wissam, Macha and Lynn are 3 people with different age, shape and gender, but they 
+                share the same passion: having a goal and aiming to reach it! Giving up is not an option and moving 
+                forward is the only way for them💪🏽
+                </p>
             </div>
-        </div>
+            <p className="categoryTitle">Legs workout</p>
+            <div className="videosDiv">
+                <div className="VideoContainer">
+                <ReactPlayer 
+                controls
+                width={"100%"}
+                height={"400px"}
+                url='https://firebasestorage.googleapis.com/v0/b/first-ecommerce-c5c38.appspot.com/o/videos%2FLynn%20Legs.mp4?alt=media&token=eb35ecfe-2b1a-44ce-9f4d-0aae8939c9ed' />
+                </div>
+
+                <div className="VideoContainer">
+                <ReactPlayer
+                controls
+                width={"100%"}
+                height={"400px"}
+                url='https://firebasestorage.googleapis.com/v0/b/first-ecommerce-c5c38.appspot.com/o/videos%2FWissam%20Legs.mp4?alt=media&token=cb309021-d0b9-4b3d-9d43-15efa0cbd8ca' />
+                </div>
+
+                </div>
+
+                <div className="videosDiv">
+                <div className="VideoContainer">
+                <ReactPlayer 
+                controls
+                width={"100%"}
+                height={"400px"}
+                url='https://firebasestorage.googleapis.com/v0/b/first-ecommerce-c5c38.appspot.com/o/videos%2FMacha%20Legs.mp4?alt=media&token=5a7405ea-ef4c-45e7-bba4-c971ceebd58c' />
+                </div>
+
+                <div className="VideoContainer">
+                <ReactPlayer
+                controls
+                width={"100%"}
+                height={"400px"}
+                url='https://firebasestorage.googleapis.com/v0/b/first-ecommerce-c5c38.appspot.com/o/videos%2FLynn%20Legs2.mp4?alt=media&token=d4ba10fb-8de0-41d5-ab8d-75efbcc3bb82' />
+                </div>
+                </div>
+
+                <div className="categorylDescription">
+                <p>
+                If you think Monday is the worst day of the week think again. Try walking down the stairs after leg day!
+                 If you can do it then you need to train harder, if not then congratulations you have passed your leg day!
+                </p>
+            </div>
+            </div>
     );
 } 
 
